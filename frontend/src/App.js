789e883e64891268505d4e25981c8da0e4c881fb
Abin babu <abinkollamparambil3@gmail.com>
2026-02-27 12:18:52 +0530

    updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/login';
 import MindEaseHome from './components/home';
 import './App.css';
 
 function App() {
-  return <MindEaseHome />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<MindEaseHome />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
